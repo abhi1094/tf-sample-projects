59a67b5ec4def5d086d0bcddfe2aff577ad290fe
abhi1094 <44882655+abhi1094@users.noreply.github.com>
2023-10-03 09:13:04 +0100

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-west-2"  # replace with your AWS region
+}
+
+variable "bucket_names" {
+  description = "List of S3 bucket names"
+  type        = list(string)
+  default     = ["bucket1", "bucket2", "bucket3", "bucket4", "bucket5"]  # replace with your bucket names
+}
+
+resource "aws_s3_bucket" "bucket" {
+  for_each = toset(var.bucket_names)
+
+  bucket = each.key
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  for_each = toset(var.bucket_names)
+
+  bucket = aws_s3_bucket.bucket[each.key].id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
